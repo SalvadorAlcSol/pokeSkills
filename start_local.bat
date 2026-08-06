92ff7c56bdba1cc5bad653116173788c7837df5b
Salvador Alcántara <s.alcantara90@gmail.com>
2026-08-06 14:22:12 -0600

    Change dev server port to 5180 to bypass Hyper-V Windows NAT reserved ports exclusions

--- start_local.bat
+++ start_local.bat
@@ -3,6 +3,6 @@ cd /d "%~dp0"
 echo ==============================================
 echo Iniciando Pokeroutes en Local...
 echo ==============================================
-start http://localhost:5173
+start http://localhost:5180
 call npm run dev
 pause
